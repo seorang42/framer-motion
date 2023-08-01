e3d4c7249dbbf903ef2642fca7af7d6bc33199d7
seorang42 <seorang42@naver.com>
2023-08-01 18:49:12 +0900

    8.9 MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import styled from "styled-components";
 import {
   motion,
   useMotionValue,
-  useMotionValueEvent,
   useTransform,
+  useScroll,
+  useMotionValueEvent,
 } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   display: flex;
   width: 100vw;
-  height: 100vh;
+  height: 200vh;
   justify-content: center;
   align-items: center;
 `;
@@ -24,12 +25,23 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  // useMotionValueEvent(x, "change", (value) => console.log(value));
-  useMotionValueEvent(scale, "change", (value) => console.log(value));
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  useMotionValueEvent(scrollYProgress, "change", (scroll) => {
+    console.log(`scrollYProgress : ${scroll}`);
+  });
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale: scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
